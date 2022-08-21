31482013bd0940119e8d6c897707eb03d3b3aca7
Alex Labanino <ghostrider6610@gmail.com>
2022-08-21 11:47:03 -0400

    OOP Constructors and Destructors

---
+++ 06/includes/person.inc.php
@@ -0,0 +1,24 @@
+<?php
+
+class Person {
+  // Properties
+  private $name;
+  private $eyeColor;
+  private $age;
+
+  // Constructor
+  public function __construct($name, $eyeColor, $age) {
+    $this->name = $name;
+    $this->eyeColor = $eyeColor;
+    $this->age = $age;
+  }
+
+  // Methods
+  public function setName($name) {
+    $this->name = $name;
+  }
+
+  public function getName() {
+    return $this->name;
+  }
+}
